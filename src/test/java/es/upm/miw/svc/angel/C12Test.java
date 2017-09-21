00c2475d243dade62da3045dd3ad1a2c43d09be2
angel <bo0477@192.168.7.19>
2017-09-21 16:51:36 +0200

    Creado C12Test

---
+++ src/test/java/es/upm/miw/svc/angel/C12Test.java
@@ -0,0 +1,23 @@
+package es.upm.miw.svc.angel;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class C12Test {
+    
+    private C12 myClass;
+
+    @Before
+    public void before() {
+        myClass = new C12();
+    }
+
+    @Test
+    public void testMA() {
+        myClass = new C12();
+        assertEquals( "C12.mA()", myClass.mA() );
+    }
+
+}

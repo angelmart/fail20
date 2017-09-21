40dee6147522c54570c211b11cfa830e3d3ede78
angel <bo0477@192.168.7.19>
2017-09-21 16:50:57 +0200

    Creado C31Test

---
+++ src/test/java/es/upm/miw/svc/angel/C31Test.java
@@ -0,0 +1,29 @@
+package es.upm.miw.svc.angel;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class C31Test {
+
+    private C31 myClass;
+    
+    @Before
+    public void before() {
+        myClass = new C31();
+    }
+
+    @Test
+    public void testM1() {
+        myClass = new C31();
+        assertEquals( "C31.m1()", myClass.m1() );
+    }
+
+    @Test
+    public void testM2() {
+        myClass = new C31();
+        assertEquals( "C31.m2()", myClass.m2() );
+    }
+
+}
